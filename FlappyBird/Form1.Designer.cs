dc0d73186841177c399857d1633a7e7966ab36f9
James Hunt <jameshuntwork1997@yahoo.com>
2017-12-16 21:43:30 +0000

    Initial setup
    nothing much, empty classes made and canvas has been created!

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picFlappyBird = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picFlappyBird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFlappyBird
+            // 
+            this.picFlappyBird.Location = new System.Drawing.Point(12, 12);
+            this.picFlappyBird.Name = "picFlappyBird";
+            this.picFlappyBird.Size = new System.Drawing.Size(1026, 730);
+            this.picFlappyBird.TabIndex = 0;
+            this.picFlappyBird.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 754);
+            this.Controls.Add(this.picFlappyBird);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFlappyBird)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFlappyBird;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        #region classes and objects used
+
+        //private Bird playerIcon;
+        //private List<Pipe> pipes = new List<Pipe>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Flappy Bird - James Hunt";
+
+            this.Height = 800;
+            this.Width = 800;
+
+            this.picFlappyBird.Height = 800;
+            this.picFlappyBird.Width = 800;
+            this.picFlappyBird.Location = new Point(0, 0);
+
+            this.KeyDown += this.Form1_KeyDown;
+        }
+
+        #endregion
+
+        #region initiate game and start timer
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.initGame();
+        }
+
+        private void initGame()
+        {
+            Timer refresh = new Timer();
+            refresh.Interval = 10;
+            refresh.Tick += new EventHandler(this.refreshGame);
+            refresh.Start();
+        }
+
+        private void refreshGame(object sender, EventArgs e)
+        {
+            this.draw();
+        }
+        
+        #endregion
+
+        #region draw method (play game)
+
+        private void draw()
+        {
+            // Create canvas
+            Graphics FlappyBird = this.picFlappyBird.CreateGraphics();
+            FlappyBird.Clear(Color.LightBlue);
+
+            // Draw pipes
+
+
+            // Draw player icon
+
+        }
+
+        #endregion
+
+        #region manage player movement
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
